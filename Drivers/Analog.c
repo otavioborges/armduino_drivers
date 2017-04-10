10f6e94b754404551a10e7f8eb950bacc9350125
Otavio Borges <otavio.car.borges@gmail.com>
2017-04-10 03:53:45 -0300

    Add support for analog read

--- Drivers/Analog.c
+++ Drivers/Analog.c
@@ -7,6 +7,12 @@
 
 #include "Analog.h"
 
+functionPtr functionADC;
+
+void ADC0_IRQHandler(void){
+	functionADC();
+}
+
 void Analog_InitPWM(void){
 	SIM->SCGC |= (SIM_SCGC_FTM0_MASK | SIM_SCGC_FTM2_MASK);	// activate FlexTimer peripherals
 
@@ -14,7 +20,6 @@ void Analog_InitPWM(void){
 																			// using CPU clock 32MHz
 	FTM2->SC = (FTM_SC_CPWMS_MASK | FTM_SC_CLKS(0x01) | FTM_SC_PS(0x00));	// center-aligned PWM
 																			// using CPU clock 32MHz
-
 	FTM0->CNTIN = 0;	// count begin at 0
 	FTM2->CNTIN = 0;
 
@@ -32,7 +37,20 @@ void Analog_InitPWM(void){
 }
 
 void Analog_InitAnalog(void){
+	SIM->SCGC |= SIM_SCGC_ADC_MASK;
 
+	ADC->SC1 = 0;
+	ADC->SC2 = 0;
+	ADC->SC3 = ADC_SC3_MODE(0x02);	// short sample, max input clock, 12-bit mode
+	ADC->SC4 = 0;					// no FIFO, no compare
+}
+
+void Analog_DeinitPWM(void){
+	SIM->SCGC &= ~(SIM_SCGC_FTM0_MASK | SIM_SCGC_FTM2_MASK);
+}
+
+void Analog_DeinitAnalog(void){
+	SIM_SCGC &= ~(SIM_SCGC_ADC_MASK);
 }
 
 void Analog_SetPWMPin(PWM pwmPin){
@@ -57,4 +75,41 @@ void Analog_Write(PWM pwmPin, uint16_t value){
 		FTM0->CONTROLS[pwmPin].CnSC = value;	// revert pin to GPIO
 	else
 		FTM2->CONTROLS[pwmPin].CnSC = value;	// revert pin to GPIO
+}
+
+void Analog_EnableIRQ(void){
+	ADC->SC1 |= ADC_SC1_AIEN_MASK;
+	NVIC_EnableIRQ(ADC0_IRQn);
+}
+
+void Analog_DisableIRQ(void){
+	ADC->SC1 &= ~(ADC_SC1_AIEN_MASK);
+	NVIC_DisableIRQ(ADC0_IRQn);
+}
+
+void Analog_SetIRQFunction(functionPtr function){
+	functionADC = function;
+}
+
+void Analog_SetPin(AnalogPin pin){
+	ADC->APCTL1 |= (1 << pin);
+}
+
+void Analog_UnsetPin(AnalogPin pin){
+	ADC->APCTL1 &= ~(1 << pin);
+}
+
+uint16_t Analog_ReadPolling(AnalogPin pin){
+	ADC->SC1 |= pin;
+
+	while((ADC->SC1 & ADC_SC1_COCO_MASK) == 0);	// wait for conversion to be complete
+	return (uint16_t)ADC->R;
+}
+
+void Analog_ReadIRQStart(AnalogPin pin){
+	ADC->SC1 |= pin;	// start pin conversion
+}
+
+uint16_t Analog_ReadIRQValue(AnalogPin pin){
+	return (uint16_t)ADC->R;
 }
--- Drivers/Analog.h
+++ Drivers/Analog.h
@@ -16,11 +16,34 @@ typedef enum{
 	PWM9 = 1u
 } PWM;
 
+typedef enum{
+	Analog0 = 11u,
+	Analog1 = 10u,
+	Analog2 = 9u,
+	Analog3 = 8u,
+	Analog4 = 7u,
+	Analog5 = 6u
+} AnalogPin;
+
+typedef void (*functionPtr)(void);
+
 void Analog_InitPWM(void);
 void Analog_InitAnalog(void);
+
+void Analog_DeinitPWM(void);
+void Analog_DeinitAnalog(void);
 
 void Analog_SetPWMPin(PWM pwmPin);
 void Analog_UnsetPWMPin(PWM pwmPin);
 void Analog_Write(PWM pwmPin, uint16_t value);
+
+void Analog_EnableIRQ(void);
+void Analog_DisableIRQ(void);
+void Analog_SetIRQFunction(functionPtr function);
+void Analog_SetPin(AnalogPin pin);
+void Analog_UnsetPin(AnalogPin pin);
+uint16_t Analog_ReadPolling(AnalogPin pin);
+void Analog_ReadIRQStart(AnalogPin pin);
+uint16_t Analog_ReadIRQValue(AnalogPin pin);
 
 #endif /* DRIVERS_ANALOG_H_ */
