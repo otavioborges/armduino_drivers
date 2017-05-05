ae839f20d73f809f388b1bb16c0b7757dabb976d
Otavio Borges <otavio.car.borges@gmail.com>
2017-05-05 13:34:42 -0300

    Add Interrupt drivers for KBI perpheral.

---
+++ Drivers/Interrupt.c
@@ -0,0 +1,75 @@
+/*
+ * Interrupt.c
+ *
+ *  Created on: 05/05/2017
+ *      Author: Otavio
+ */
+
+#include "Interrupt.h"
+
+functionPtr KBIfunction;
+
+void KBI0_IRQHandler(void){
+	KBIfunction(0x00);
+	KBI0->SC |= KBI_SC_KBACK_MASK;
+}
+
+void KBI0_IRQHandler(void){
+	KBIfunction(0x01);
+	KBI0->SC |= KBI_SC_KBACK_MASK;
+}
+
+void Interrupt_Init(uint8_t enableIRQ, DetectionMode mode){
+	SIM->SCGC |= (SIM_SCGC_KBI0_MASK | SIM_SCGC_KBI1_MASK);
+
+	KBI0->SC = 0;
+	KBI1->SC = 0;
+
+	if(enableIRQ){
+		KBI0->SC |= KBI_SC_KBIE_MASK;
+		KBI1->SC |= KBI_SC_KBIE_MASK;
+	}
+	if(mode == EdgesAndLevels){
+		KBI0->SC |= KBI_SC_KBMOD_MASK;
+		KBI1->SC |= KBI_SC_KBMOD_MASK;
+	}
+}
+
+void Interrupt_EnablePin(InterruptPin pin, EdgeSelect edge){
+	KBI_Type *kbiPtr;
+	if((0xFF00 & pin) == 0)
+		kbiPtr = KBI0;
+	else
+		kbiPtr = KBI1;
+
+	kbiPtr->PE |= (uint8_t)pin;
+	if(edge == RiseHighLevel)
+		kbiPtr->ES |= (uint8_t)pin;
+	else
+		kbiPtr->ES &= ~((uint8_t)pin);
+}
+
+void Interrupt_DisablePin(InterruptPin pin){
+	KBI_Type *kbiPtr;
+	if((0xFF00 & pin) == 0)
+		kbiPtr = KBI0;
+	else
+		kbiPtr = KBI1;
+
+	kbiPtr->PE &= ~((uint8_t)pin);
+}
+
+uint8_t Interrupt_PendingIRQ(void){
+	uint8_t result = 0;
+
+	if((KBI0->SC & KBI_SC_KBF_MASK))
+		result |= 0x01;
+	if((KBI1->SC & KBI_SC_KBF_MASK))
+		result |= 0x02;
+
+	return result;
+}
+
+void Interrupt_SetIRQFunction(functionPtr function){
+	KBIfunction = function;
+}
---
+++ Drivers/Interrupt.h
@@ -0,0 +1,47 @@
+/*
+ * Interrupt.h
+ *
+ *  Created on: 05/05/2017
+ *      Author: Otavio
+ */
+
+#ifndef DRIVERS_INTERRUPT_H_
+#define DRIVERS_INTERRUPT_H_
+
+#include "MKE02Z2.h"
+
+typedef enum {
+	IntPin0  = 0x0001,
+	IntPin1  = 0x0002,
+	IntPin2  = 0x0004,
+	IntPin3  = 0x0008,
+	IntPin4  = 0x0010,
+	IntPin5  = 0x0020,
+	IntPin6  = 0x0040,
+	IntPin7  = 0x0080,
+	IntPin8  = 0x0101,
+	IntPin9  = 0x0102,
+	IntPin10 = 0x0104,
+	IntPin11 = 0x0108,
+	IntPin12 = 0x0110,
+} InterruptPin;
+
+typedef enum {
+	Edges,
+	EdgesAndLevels
+} DetectionMode;
+
+typedef enum {
+	FallingLowLevel,
+	RiseHighLevel
+} EdgeSelect;
+
+typedef void (*functionPtr)(uint8_t);
+
+void Interrupt_Init(uint8_t enableIRQ, DetectionMode mode);
+void Interrupt_EnablePin(InterruptPin pin, EdgeSelect edge);
+void Interrupt_DisablePin(InterruptPin pin);
+uint8_t Interrupt_PendingIRQ(void);
+void Interrupt_SetIRQFunction(functionPtr function);
+
+#endif /* DRIVERS_INTERRUPT_H_ */
